98d2f5c3f627208f8326fa061e7a034c12b619dd
maue-93 <morazara.c@gmail.com>
2024-06-21 11:57:42 -0400

    added profile model

--- harmony/models.py
+++ harmony/models.py
@@ -1,3 +1,27 @@
+from uuid import uuid4
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+"""
+
+"""
+class UserProfile (models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    # delete user when user profile is deleted
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile", unique=True)
+    bio = models.TextField(null=True, blank=True)
+    picture = models.ImageField(upload_to="users/images")
+
+# end of UserProfile
+
+"""
+
+"""
+
+
+
+
+
+
